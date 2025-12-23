deba3ed6947b314e13a2027c150aab97c3887cf6
AiRanthem <zhongtianyun.zty@alibaba-inc.com>
2025-12-23 10:53:57 +0800

    support customized e2b
    Signed-off-by: AiRanthem <zhongtianyun.zty@alibaba-inc.com>

--- sdk/customized_e2b/demo.py
+++ sdk/customized_e2b/demo.py
@@ -2,22 +2,21 @@ import time
 import sys
 
 from dotenv import load_dotenv
-
 load_dotenv()
-# Import the E2B SDK
-from kruise_agents.e2b import Sandbox
-# from e2b_code_interpreter import Sandbox
 
-# Create a sandbox using the E2B Python SDK
-# Connect to the local E2B on Kubernetes server
+# Patch and import the E2B SDK
+from kruise_agents.e2b import patch_e2b
+from e2b_code_interpreter import Sandbox
+patch_e2b()
 
 try:
     start = time.time()
     sbx: Sandbox = Sandbox.create(template="code-interpreter", timeout=300, metadata={
-       "foo": "bar"
+        "foo": "bar"
     })
     end = time.time()
     print(f"sandbox id: {sbx.sandbox_id}", file=sys.stderr)
+    print(f"sandbox url: {sbx.sandbox_domain}")
     # Connect to the sandbox (it will automatically resume the sandbox, if paused)
     same_sbx = Sandbox.connect(sbx.sandbox_id)
     print('Connected to the sandbox', same_sbx.sandbox_id, file=sys.stderr)
@@ -36,9 +35,8 @@ def execute_python_code(s: Sandbox, code: str):
         print(execution.logs.stdout)
 
 
-
 try:
-   execute_python_code(sbx, """
+    execute_python_code(sbx, """
 def bubble_sort(arr):
     n = len(arr)
     # 创建数组副本，避免修改原数组
--- sdk/customized_e2b/kruise_agents/e2b.py
+++ sdk/customized_e2b/kruise_agents/e2b.py
@@ -1,69 +1,24 @@
 import os
-from typing import Optional, Dict, Unpack, Self
 
-from e2b.connection_config import ApiParams
-from e2b.sandbox.sandbox_api import McpServer, SandboxNetworkOpts
-from e2b_code_interpreter import Sandbox as E2bSandbox
-from e2b.sandbox.utils import class_method_variant
+from e2b import ConnectionConfig
+from e2b.sandbox.main import SandboxBase
 
 
-class Sandbox(E2bSandbox):
-    def get_host(self, port: int) -> str:
-        return f"sandbox.{self.sandbox_domain}/kruise/{self.sandbox_id}/{port}"
-
-    @classmethod
-    def create(
-            cls,
-            template: Optional[str] = None,
-            timeout: Optional[int] = None,
-            metadata: Optional[Dict[str, str]] = None,
-            envs: Optional[Dict[str, str]] = None,
-            secure: bool = True,
-            allow_internet_access: bool = True,
-            mcp: Optional[McpServer] = None,
-            network: Optional[SandboxNetworkOpts] = None,
-            https: bool = True,
-            **opts: Unpack[ApiParams],
-    ) -> Self:
-        sbx: E2bSandbox = super().create(
-            template=template,
-            timeout=timeout,
-            metadata=metadata,
-            envs=envs,
-            secure=secure,
-            allow_internet_access=allow_internet_access,
-            mcp=mcp,
-            network=network,
-            api_url=get_api_url(https),
-            **opts,
-        )
-        if not https:
-            sbx.connection_config.debug = True
-        return sbx
-
-    @class_method_variant("_cls_connect")
-    def connect(
-            self,
-            timeout: Optional[int] = None,
-            https: bool = True,
-            **opts: Unpack[ApiParams],
-    ) -> Self:
-        return super().connect(
-            timeout,
-            api_url=get_api_url(https),
-            **opts,
-        )
-
-    @classmethod
-    def _cls_connect(
-            cls,
-            sandbox_id: str,
-            timeout: Optional[int] = None,
-            https: bool = True,
-            **opts: Unpack[ApiParams],
-    ) -> Self:
-        return super()._cls_connect(sandbox_id, timeout, api_url=get_api_url(https), **opts)
+def __sandbox_get_host(self, port: int) -> str:
+    return f"{self.sandbox_domain}/kruise/{self.sandbox_id}/{port}"
 
 
-def get_api_url(https: bool):
+def __connection_config_get_host(_, sandbox_id: str, sandbox_domain: str, port: int) -> str:
+    return f"{sandbox_domain}/kruise/{sandbox_id}/{port}"
+
+
+def __get_api_url(https: bool):
     return f"{'https' if https else 'http'}://{os.environ['E2B_DOMAIN']}/kruise/api"
+
+
+def patch_e2b(https: bool = True):
+    os.environ["E2B_API_URL"] = __get_api_url(https)
+    if not https:
+        os.environ["E2B_DEBUG"] = "true"
+    SandboxBase.get_host = __sandbox_get_host
+    ConnectionConfig.get_host = __connection_config_get_host
