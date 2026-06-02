8a606384a00f8c04695b7922af543d79ac832915
AiRanthem <zhongtianyun.zty@alibaba-inc.com>
2026-06-02 15:33:09 +0800

    fix test
    Signed-off-by: AiRanthem <zhongtianyun.zty@alibaba-inc.com>

--- pkg/controller/sandboxupdateops/sandboxupdateops_controller.go
+++ pkg/controller/sandboxupdateops/sandboxupdateops_controller.go
@@ -24,6 +24,7 @@ import (
 	"reflect"
 	"sort"
 
+	"github.com/openkruise/agents/pkg/utils"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -40,7 +41,6 @@ import (
 	agentsv1alpha1 "github.com/openkruise/agents/api/v1alpha1"
 	"github.com/openkruise/agents/pkg/discovery"
 	"github.com/openkruise/agents/pkg/utils/expectations"
-	"github.com/openkruise/agents/pkg/utils/sandboxutils"
 )
 
 const finalizerName = "agents.kruise.io/sandboxupdateops-protection"
@@ -231,7 +231,7 @@ func (r *Reconciler) classifySandboxes(sandboxList *agentsv1alpha1.SandboxList, 
 		}
 
 		// Skip sandboxes controlled by SandboxSet (pool sandboxes, not intended for update)
-		if sandboxutils.IsControlledBySandboxSet(sbx) {
+		if utils.IsControlledBySandboxSet(sbx) {
 			continue
 		}
 
